a4f9a81f0ca80dd1af154a0358fb180381e44883
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-30 16:23:50 +0600

    image ratio

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@ use image::{open, GenericImageView};
 use std::path::PathBuf;
 
 fn main() {
-    let path = PathBuf::from("/home/remon/Downloads/octocat.png");
-    // let path = PathBuf::from("/home/remon/Pictures/pxfuel.jpg");
-    // let path = PathBuf::from("/home/remon/Pictures/10405.Longest Common Subsequence.png");
+    let path = PathBuf::from("/home/remon/Pictures/pxfuel.jpg");
     let img = open(path).unwrap();
     let (width, height) = img.dimensions();
+
     let b = img.to_rgba8().into_raw();
-    let blurhash = encode(4, 3, width, height, &b);
+    let blurhash = encode(9, 9, width, height, &b);
 
     match blurhash {
         Ok(blurhash) => {
             println!("{}", blurhash);
-            let pixels = decode(&blurhash, 300, 300, 1.0);
+            let (width, height) = dimensions(width, height);
+            println!("{}x{}", width, height);
+            let pixels = decode(&blurhash, width, height, 1.0);
             match pixels {
-                Ok(pixels) => match image::RgbaImage::from_vec(300, 300, pixels) {
+                Ok(pixels) => match image::RgbaImage::from_vec(width, height, pixels) {
                     Some(img) => {
                         img.save("test.png").unwrap();
                     }
@@ -29,4 +30,19 @@ fn main() {
         }
         Err(err) => println!("{}", err),
     }
+}
+
+fn dimensions(w: u32, h: u32) -> (u32, u32) {
+    let pixels = 50; // ? 100x100 pixels
+
+    // ? ">" ? pixels will be min and max will be calculated
+    // ? "<" ? pixels will be max and min will be calculated
+    let result = if w > h {
+        let ratio = (w as f32) / (h as f32);
+        ((pixels as f32 * ratio) as u32, pixels)
+    } else {
+        let ratio = (h as f32) / (w as f32);
+        (pixels, (pixels as f32 * ratio) as u32)
+    };
+    result
 }
